83455ac3a1d8c08bf52e385253b70313285195b0
Alexthw <77152778+Alexthw46@users.noreply.github.com>
2025-09-07 11:48:11 +0200

    tagwork, up sauce

--- src/main/java/com/alexthw/ars_hex/datagen/AHItemTagProvider.java
+++ src/main/java/com/alexthw/ars_hex/datagen/AHItemTagProvider.java
@@ -2,11 +2,17 @@ package com.alexthw.ars_hex.datagen;
 
 import com.alexthw.ars_hex.ArsHex;
 import com.alexthw.ars_hex.hexerei.HexereiCompat;
+import com.alexthw.ars_hex.malum.MalumCompat;
+import com.hollingsworth.arsnouveau.common.datagen.BlockTagProvider;
 import com.hollingsworth.arsnouveau.common.datagen.ItemTagProvider;
+import com.sammy.malum.registry.common.MalumTags;
+import net.joefoxe.hexerei.block.ModBlocks;
+import net.joefoxe.hexerei.item.ModItems;
 import net.joefoxe.hexerei.util.HexereiTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.NotNull;
@@ -23,7 +29,9 @@ public class AHItemTagProvider extends ItemTagsProvider {
     @Override
     protected void addTags(HolderLookup.@NotNull Provider provider) {
         tag(HexereiTags.Items.BROOM_BRUSH).addOptional(HexereiCompat.MAGEBLOOM_BRUSH.getId());
-        tag(ItemTagProvider.SHARD_TAG);
+        tag(ItemTagProvider.SHARD_TAG).addOptional(ModItems.SELENITE_SHARD.getId());
+        tag(MalumTags.ItemTags.MAGIC_CAPABLE_WEAPONS).addOptional(MalumCompat.ENCHANTER_SCYTHE.getId());
+        tag(Tags.Items.MELEE_WEAPON_TOOLS).addOptional(MalumCompat.ENCHANTER_SCYTHE.getId());
     }
 
     @Override
@@ -42,6 +50,8 @@ class AHBlockTagsProvider extends BlockTagsProvider {
     @Override
     protected void addTags(HolderLookup.@NotNull Provider provider) {
         // add tags here
+        tag(BlockTagProvider.BUDDING_BLOCKS).addOptional(ModBlocks.BUDDING_SELENITE.getId());
+        tag(BlockTagProvider.CLUSTER_BLOCKS).addOptional(ModBlocks.SELENITE_CLUSTER.getId());
     }
 
 }
